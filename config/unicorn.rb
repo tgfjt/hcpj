ac373b0642e6958f586170c35701906e2e4aff1a
daisuke.kohgami <daisuke.kohgami@gmail.com>
2014-07-16 17:47:24 +0900

    add unicorn

---
+++ config/unicorn.rb
@@ -0,0 +1,35 @@
+app_path = '/var/www/hcpj'
+app_shared_path = "#{app_path}/shared"
+
+worker_processes 5
+
+# 実態は symlink。
+# SIGUSR2 を送った時にこの symlink に対して
+# Unicorn のインスタンスが立ち上がる
+working_directory "#{app_path}/current/"
+
+listen "#{app_shared_path}/tmp/sockets/unicorn.sock"
+
+stdout_path "#{app_shared_path}/log/unicorn.stdout.log"
+stderr_path "#{app_shared_path}/log/unicorn.stderr.log"
+
+pid "#{app_shared_path}/tmp/pids/unicorn.pid"
+
+# ダウンタイムをなくす
+preload_app true
+
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+
+  old_pid = "#{ server.config[:pid] }.oldbin"
+  unless old_pid == server.pid
+    begin
+      Process.kill :QUIT, File.read(old_pid).to_i
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
+end
+
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+end
