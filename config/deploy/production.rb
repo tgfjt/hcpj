065f9ac1f3ab4679ba15d63b5089e8800b98fa2a
daisuke.kohgami <daisuke.kohgami@gmail.com>
2014-07-16 22:06:31 +0900

    modify search textbox

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,8 +4,10 @@ role :db, %w{apluser@54.92.86.76}
 
 set :stage, :production
 set :branch, 'master'
-set :rails_env, 'production'
-set :unicorn_rack_env, 'production'
+# set :rails_env, 'production'
+# set :unicorn_rack_env, 'production'
+set :rails_env, 'development'
+set :unicorn_rack_env, 'development'
 
 server '54.92.86.76', user: 'apluser', roles: %w{web app db}, my_property: :my_value
 
