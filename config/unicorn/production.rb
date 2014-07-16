551b50393b65255679efbda5737b22423f871f5f
daisuke.kohgami <daisuke.kohgami@gmail.com>
2014-07-16 16:40:14 +0900

    change pid

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -22,16 +22,24 @@ preload_app true
 # check_client_connection false
 
 before_fork do |server, worker|
-	old_pid = "#{RAILS_ROOT}/tmp/pids/unicorn.pid.oldbin"
-	if File.exists?(old_pid) && server.pid != old_pid
-		begin
-			Process.kill("QUIT", File.read(old_pid).to_i)
-		rescue Errno::ENOENT, Errno::ESRCH
-		end
-	end
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.connection.disconnect!
+  end
+
+  old_pid = "#{server.config[:pid] }.oldbin"
+  if old_pid != server.pid
+    begin
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
+
+  sleep 1
 end
 
 after_fork do |server, worker|
-	defined?(ActiveRecord::Base) and
-			ActiveRecord::Base.establish_connection
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.establish_connection
+  end
 end
