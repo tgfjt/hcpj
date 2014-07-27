9f514da86f5b280fdaf78cda4e08df6ef5e83208
daisuke.kohgami <daisuke.kohgami@gmail.com>
2014-07-28 01:45:59 +0900

    add favorites unique index

--- db/migrate/20140721202453_add_index_to_favorite.rb
+++ db/migrate/20140721202453_add_index_to_favorite.rb
@@ -1,5 +1,5 @@
 class AddIndexToFavorite < ActiveRecord::Migration
   def change
-    add_index :favorites, [:project_id, :talent_id], unique: true
+    add_index :favorites, [:project_id, :talent_id, :user_id], unique: true
   end
 end
